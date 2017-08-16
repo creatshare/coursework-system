f2b895a4ad50f301f136a767705294c549bb1d8b
hylerrix <hylerrix@foxmail.com>
2017-08-17 02:28:48 +0800

    [hanyile]app router connected with each component

--- src/index.js
+++ src/index.js
@@ -16,20 +16,11 @@ import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
     <Router>
-      <div>
-        <ul>
-          <li><Link to="/">Login</Link></li>
-          <li><Link to="/app/home">HomeContainer</Link></li>
-          <li><Link to="/app/profile">ProfileContainer</Link></li>
-          <li><Link to="/404">NotFound</Link></li>
-        </ul>
-        <hr/>
         <Switch>
           <Route exact path="/" component={LoginContainer}/>
           <Route path="/app" component={HomeContainer}/>
           <Route component={NotFound}/>
         </Switch>
-      </div>
     </Router>,
     document.getElementById('app')
 )
