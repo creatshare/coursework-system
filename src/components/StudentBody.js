a4db512a8ef49207a573992600552a0486b8d6cd
icorvoh <icorvoh@qq.com>
2017-08-17 14:04:18 +0800

    [hanyile]student`s ui had done

--- src/components/StudentBody.js
+++ src/components/StudentBody.js
@@ -8,7 +8,7 @@ import { Grid } from 'semantic-ui-react'
 import TaskWrap from './TaskWrap'
 import ListWrap from './ListWrap'
 
-const HomeBody = () => (
+const StudentBody = () => (
     <Grid divided='vertically' className="BodyGrid">
       <Grid.Row columns={2}>
         <Grid.Column mobile={16} tablet={16} computer={8}>
@@ -22,4 +22,4 @@ const HomeBody = () => (
     </Grid>
 )
 
-export default HomeBody
+export default StudentBody
---
+++ src/container/TeacherContainer.js
@@ -0,0 +1,17 @@
+/**
+ * Created by hanyile on 2017/8/17.
+ */
+
+import React, {Component} from 'react'
+
+class TeacherContainer extends Component {
+  render () {
+    return (
+        <div>
+          Teacher
+        </div>
+    )
+  }
+}
+
+export default TeacherContainer
