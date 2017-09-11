358be11437f034c13c478605f8f401287b1ae6e2
hylerrix <hylerrix@foxmail.com>
2017-09-11 14:17:46 +0800

    [hanyile]now student can see theirs history work

--- src/components/student/ItemModal.js
+++ src/components/student/ItemModal.js
@@ -2,45 +2,86 @@
  * Created by hanyile on 2017/9/5.
  */
 
-import React from 'react'
+import React, { Component} from 'react'
 import { Header, Modal, Button, Icon } from 'semantic-ui-react'
+import request from 'superagent'
 import AceEditor from 'react-ace'
 import 'brace/mode/c_cpp'
 import 'brace/theme/github'
 
-const ItemModal = () => (
-    <Modal trigger={<a href="javascript: void(0)">A (Read more)</a>}>
-      <Modal.Header>C Language System - A</Modal.Header>
-      <Modal.Content image scrolling>
-        <Modal.Description>
-          <Header>Type</Header>
-          <p>Home Work</p>
-          <Header>Submit Time</Header>
-          <p>2017-08-01</p>
-          <Header>Your Code</Header>
-          <AceEditor
-            style={{
-              height: '22vh',
-              width: '100%'
-            }}
-            mode="c_cpp"
-            theme="github"
-            editorProps={{$blockScrolling: true}}
-            readOnly="true"
-            value={"#include \<stido.h\>\n\nint main (void) \{\n    printf(\"Hello World!\");\n    return 0;\n\}"}
-          />
-          <Header>Comment</Header>
-          <p>This is an example of expanded content that will cause the modal's dimmer to scroll</p>
-          <p>This is an example of expanded content that will cause the modal's dimmer to scroll</p>
-          <p>This is an example of expanded content that will cause the modal's dimmer to scroll</p>
-        </Modal.Description>
-      </Modal.Content>
-      <Modal.Actions>
-        <Button primary>
-          Proceed <Icon name='right chevron' />
-        </Button>
-      </Modal.Actions>
-    </Modal>
-)
+class ItemModal extends Component {
+  constructor (props) {
+    super(props)
+    let { workTypeID } = props
+    this.state = {
+      id: sessionStorage.getItem("id"),
+      token: sessionStorage.getItem("token"),
+      workTypeID: workTypeID,
+      submitTime: "",
+      workTitle: "",
+      workContent: "",
+      workRating: "",
+      workComment: ""
+    }
+  }
+
+  componentDidMount () {
+    request.post('http://222.24.63.100:9138/cms/getworkdetail')
+      .type('form')
+      .send({
+        id: this.state.id,
+        token: this.state.token,
+        workid: this.state.workTypeID
+      })
+      .end((err, data)=> {
+        let res = data.text.split("`")
+        let workRating = res[4] !== " " ? res[4] : "-"
+        this.setState({
+          submitTime: res[0],
+          workTypeID: res[1],
+          workTitle: res[2],
+          workContent: res[3],
+          workRating: workRating,
+          workComment: res[5]
+        })
+      })
+  }
+
+  render () {
+    return (
+        <Modal trigger={<a href="javascript: void(0)">{this.state.workRating} (Read more)</a>}>
+          <Modal.Header>C Language System - {this.state.workRating !== "-" ? this.state.workRating : "No rating from teachers yet"}</Modal.Header>
+          <Modal.Content image scrolling>
+            <Modal.Description>
+              <Header>Type</Header>
+              <p>{this.state.workTypeID}</p>
+              <Header>Submit Time</Header>
+              <p>{this.state.submitTime}</p>
+              <Header>Your Code</Header>
+              <AceEditor
+                  style={{
+                    height: '22vh',
+                    width: '100%'
+                  }}
+                  mode="c_cpp"
+                  theme="github"
+                  editorProps={{$blockScrolling: true}}
+                  readOnly="true"
+                  value={this.state.workContent}
+              />
+              <Header>Comment</Header>
+              <p>{this.state.workComment !== " " ? this.state.workComment : "No comment from teachers yet."}</p>
+            </Modal.Description>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button primary>
+              Proceed <Icon name='right chevron' />
+            </Button>
+          </Modal.Actions>
+        </Modal>
+    )
+  }
+}
+
 
 export default ItemModal
--- src/components/student/ListWrap.js
+++ src/components/student/ListWrap.js
@@ -2,74 +2,109 @@
  * Created by hanyile on 2017/8/15.
  */
 
-import React from 'react'
-import { Container, Table, Header, Form, Select, Segment } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Container, Table, Header, Form, Select, Segment, Dimmer, Loader } from 'semantic-ui-react'
+import request from 'superagent'
 import DatePicker from 'react-datepicker'
 import ItemModal from './ItemModal'
+import createHistory from 'history/createBrowserHistory'
 
-import 'react-datepicker/dist/react-datepicker.css'
+const history = createHistory()
 
-const ListWrap = () => (
-    <Container className="ListContainer">
-      <Segment className="ListSegment">
+class ListWrap extends Component {
+  constructor () {
+    super()
+    this.state = {
+      id: sessionStorage.getItem("id"),
+      token: sessionStorage.getItem("token"),
+      workTypeID: "2",
+      dateFrom: "2017-09-01",
+      dateTo: "2017-09-11",
+      myAllWork: []
+    }
+  }
 
-        {/*<Dimmer active inverted>*/}
-        {/*<Loader indeterminate>Loading the data</Loader>*/}
-        {/*</Dimmer>*/}
+  componentDidMount () {
+    request.post('http://222.24.63.100:9138/cms/searchmywork')
+      .type('form')
+      .send({
+        id: this.state.id,
+        token: this.state.token,
+        typeid: this.state.workTypeID,
+        datefrom: this.state.dateFrom,
+        dateto: this.state.dateTo
+      })
+      .end((err, data)=> {
+        if (data.text === "0") return
+        let myAllWork = data.text.split("`")
+        myAllWork.pop()
+        this.setState({
+          myAllWork: myAllWork
+        })
+      })
+  }
 
-        <Header as='h1'>History</Header>
+  render () {
+    return (
+        <Container className="ListContainer">
+          <Segment className="ListSegment">
 
-        <Form>
-          <Form.Field control={Select} label='Type' options={[
-            {key: 'al', text: 'All Type', value: 'al'},
-            {key: 'ho', text: 'Home Work', value: 'ho'},
-            {key: 'co', text: 'Couse Work', value: 'co'},
-            {key: 'cl', text: 'Class Work', value: 'cl'},
-            {key: 'ot', text: 'Other', value: 'ot'},
-          ]} placeholder='Select Your Work Type' />
-          <Form.Group widths='equal'>
-            <Form.Field control={DatePicker} label='From Date' options={[
-              {key: 'fd', text: '2017-08-01', value: 'fd'}
-            ]} placeholder='Select Date' className="ListDateField"/>
-            <Form.Field control={DatePicker} label='To' options={[
-              {key: 'td', text: '2017-08-01', value: 'td'}
-            ]} placeholder='Select Date' className="ListDateField"/>
-          </Form.Group>
-        </Form>
+            <Dimmer inverted>
+              <Loader indeterminate>Loading the data</Loader>
+            </Dimmer>
 
-        <Table celled>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Submit Time</Table.HeaderCell>
-              <Table.HeaderCell>Type</Table.HeaderCell>
-              <Table.HeaderCell>Title</Table.HeaderCell>
-              <Table.HeaderCell>Rating</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
+            <Header as='h1'>History</Header>
 
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>2017-08-01</Table.Cell>
-              <Table.Cell>Home Work</Table.Cell>
-              <Table.Cell>C Language System</Table.Cell>
-              <Table.Cell><ItemModal/></Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>2017-08-01</Table.Cell>
-              <Table.Cell>Home Work</Table.Cell>
-              <Table.Cell>C Language System</Table.Cell>
-              <Table.Cell><ItemModal/></Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>2017-08-01</Table.Cell>
-              <Table.Cell>Home Work</Table.Cell>
-              <Table.Cell>C Language System</Table.Cell>
-              <Table.Cell><ItemModal/></Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
-      </Segment>
-    </Container>
-)
+            <Form>
+              <Form.Field control={Select} label='Type' options={[
+                {key: 'al', text: 'All Type', value: 'al'},
+                {key: 'ho', text: 'Home Work', value: 'ho'},
+                {key: 'co', text: 'Couse Work', value: 'co'},
+                {key: 'cl', text: 'Class Work', value: 'cl'},
+                {key: 'ot', text: 'Other', value: 'ot'},
+              ]} placeholder='Select Your Work Type' />
+              <Form.Group widths='equal'>
+                <Form.Field control={DatePicker} label='From Date' options={[
+                  {key: 'fd', text: '2017-08-01', value: 'fd'}
+                ]} placeholder='Select Date' className="ListDateField"/>
+                <Form.Field control={DatePicker} label='To' options={[
+                  {key: 'td', text: '2017-08-01', value: 'td'}
+                ]} placeholder='Select Date' className="ListDateField"/>
+              </Form.Group>
+            </Form>
+
+            <Table celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Submit Time</Table.HeaderCell>
+                  <Table.HeaderCell>Type</Table.HeaderCell>
+                  <Table.HeaderCell>Title</Table.HeaderCell>
+                  <Table.HeaderCell>Rating</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+
+              <Table.Body>
+                {(() => {
+                  let divContainerArr = []
+                  this.state.myAllWork.forEach((value, index, array) => {
+                    if (index % 6 !== 0) return
+                    divContainerArr.push(
+                        <Table.Row>
+                          <Table.Cell>{array[index + 1]}</Table.Cell>
+                          <Table.Cell>{array[index + 2]}</Table.Cell>
+                          <Table.Cell>{array[index + 3]}</Table.Cell>
+                          <Table.Cell><ItemModal workTypeID={array[index]} /></Table.Cell>
+                        </Table.Row>
+                    )
+                  })
+                  return divContainerArr
+                })()}
+              </Table.Body>
+            </Table>
+          </Segment>
+        </Container>
+    )
+  }
+}
 
 export default ListWrap
